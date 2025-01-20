4bf319b9ae311d98e6d8e0d5f674329c8ff12a9d
Debopriyo Das <70844598+Dragon4926@users.noreply.github.com>
2025-01-21 02:07:20 +0530

    lgtm ✅

--- js/server.js
+++ js/server.js
@@ -66,7 +66,7 @@ app.use((err, req, res, next) => {
 
 // SPA fallback
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
